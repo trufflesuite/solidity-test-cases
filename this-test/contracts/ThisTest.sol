3dac8cee8593790fac7285b955a7d70e0d1b6116
Harry Altman <harry.j.altman@gmail.com>
2019-02-12 14:26:40 -0500

    Add more tests of libraries, "this", and the trace

--- this-test/contracts/ThisTest.sol
+++ this-test/contracts/ThisTest.sol
@@ -1,20 +1,52 @@
-pragma solidity ~0.5;
+pragma solidity ^0.5.0;
 
 contract Tester {
 
+  event Done();
+
   ThisTest nonself;
+
+  address[1] receiver;
+
+  uint[2] buffer;
 
   function run() public {
     nonself = new ThisTest();
+  }
+
+  function runLib() public {
+    ThisTestLib.store(receiver);
+    emit Done();
+  }
+
+  function testTrace() public {
+    uint sum;
+    buffer[0] = 33;
+    buffer[1] = 35;
+    sum = ThisTestLib.add(buffer);
+    emit Done();
   }
 }
 
 contract ThisTest {
 
+  event Done();
+
   ThisTest public self;
 
   constructor() public {
     self = this;
+    emit Done();
   }
 
+}
+
+library ThisTestLib {
+  function store(address[1] storage receiver) external {
+    receiver[0] = address(this);
+  }
+
+  function add(uint[2] storage summands) external returns (uint) {
+    return summands[0] + summands[1];
+  }
 }
--- this-test/migrations/2_deploy_contracts.js
+++ this-test/migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var Tester = artifacts.require("Tester");
+var ThisTestLib = artifacts.require("ThisTestLib");
 
 module.exports = function(deployer) {
+  deployer.deploy(ThisTestLib);
+  deployer.link(ThisTestLib, Tester);
   deployer.deploy(Tester);
 };
