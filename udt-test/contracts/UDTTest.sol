967bb8ea41994fde2c70cdd44c1b2a0a02bc9d20
Harry Altman <harry.j.altman@gmail.com>
2019-02-14 18:14:57 -0500

    Add some types with special characters in the names!

--- udt-test/contracts/UDTTest.sol
+++ udt-test/contracts/UDTTest.sol
@@ -15,3 +15,47 @@ interface UserInterface {
 
 library UserLibrary {
 }
+
+contract Underscore_Contract {
+  struct Underscore_Struct {
+    uint val;
+  }
+
+  enum Underscore_Enum {
+    it
+  }
+
+  Underscore_Struct theStruct;
+  Underscore_Enum theEnum;
+  Underscore_Contract theContract;
+  Underscore_Interface theInterface;
+  Underscore_Library theLibrary;
+}
+
+interface Underscore_Interface {
+}
+
+library Underscore_Library {
+}
+
+contract Dollar$Contract {
+  struct Dollar$Struct {
+    uint val;
+  }
+
+  enum Dollar$Enum {
+    it
+  }
+
+  Dollar$Struct theStruct;
+  Dollar$Enum theEnum;
+  Dollar$Contract theContract;
+  Dollar$Interface theInterface;
+  Dollar$Library theLibrary;
+}
+
+interface Dollar$Interface {
+}
+
+library Dollar$Library {
+}
