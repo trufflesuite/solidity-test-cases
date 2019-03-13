495186745b1b7c70bf7e36270a91a53ecf9c6d01
Harry Altman <harry.j.altman@gmail.com>
2019-03-13 18:02:42 -0400

    Add tests demonstrating that we fail to distinguish between failed creations

--- creation-failure/contracts/CantCreate.sol
+++ creation-failure/contracts/CantCreate.sol
@@ -5,3 +5,27 @@ contract CantCreate {
     revert("Nope!");
   }
 }
+
+contract MappingMixup {
+
+  mapping(uint => uint) mixup;
+
+  constructor(uint n, Tester invoker) public {
+    mixup[n] = 0;
+    if(n > 0) {
+      invoker.mixup(n - 1);
+    }
+    else {
+      revert("The inevitable reversion!");
+    }
+  }
+}
+
+contract Tester {
+
+  MappingMixup mappingMixup;
+  
+  function mixup(uint n) public {
+    mappingMixup = new MappingMixup(n, this);
+  }
+}
--- creation-failure/migrations/2_deploy_contracts.js
+++ creation-failure/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var CantCreate = artifacts.require("CantCreate");
+var Tester = artifacts.require("Tester");
 
 module.exports = function(deployer) {
-  deployer.deploy(CantCreate);
+  //deployer.deploy(CantCreate);
+  deployer.deploy(Tester);
 };
