f79f204765c0016c5a870f5106e843a96fdcca0d
Harry Altman <harry.j.altman@gmail.com>
2019-03-25 22:33:43 -0400

    Add reversion test using assert(false)

--- reversion-test/contracts/RevertTest.sol
+++ reversion-test/contracts/RevertTest.sol
@@ -16,3 +16,20 @@ contract RevertTest {
     y = x;
   }
 }
+
+contract RevertTest2 {
+
+  uint x;
+  uint y;
+
+  function() external {
+    x = 2;
+    assert(false);
+  }
+
+  function run() public {
+    x = 1;
+    address(this).call.gas(gasleft()/2)(hex"");
+    y = x;
+  }
+}
--- reversion-test/migrations/2_deploy_contracts.js
+++ reversion-test/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var RevertTest = artifacts.require("RevertTest");
+var RevertTest2 = artifacts.require("RevertTest2");
 
 module.exports = function(deployer) {
   deployer.deploy(RevertTest);
+  deployer.deploy(RevertTest2);
 };
