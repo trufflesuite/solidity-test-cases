236559b881dfb063bb275c68e36392cdd2d50331
Harry Altman <harry.j.altman@gmail.com>
2019-03-29 01:26:06 -0400

    A test of what happens when you manually call the zero address

---
+++ manual-zero-test/contracts/ManualZeroTest.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.7;
+
+contract ManualZeroTest {
+
+  bool status;
+  bytes result;
+
+  function run() public {
+    address zero = 0x0000000000000000000000000000000000000000;
+    (status, result) = zero.call(hex"7f602a60005360016000f300000000000000000000000000000000000000000000600052600a6000f3");
+  }
+}
---
+++ manual-zero-test/migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var ManualZeroTest = artifacts.require("ManualZeroTest");
+
+module.exports = function(deployer) {
+  deployer.deploy(ManualZeroTest);
+};
