6f92b3699cc5862dea35d5f837956dee593157e9
Harry Altman <harry.j.altman@gmail.com>
2019-04-16 03:05:37 -0400

    Remove defunct test

--- internals-test/migrations/2_deploy_contracts.js
+++ internals-test/migrations/2_deploy_contracts.js
@@ -2,7 +2,6 @@ var InvalidTest = artifacts.require("InvalidTest");
 var InvalidLibTest = artifacts.require("InvalidLibTest");
 var InvalidLib = artifacts.require("InvalidLib");
 var ConstructorTest = artifacts.require("ConstructorTest");
-var MemoryTest = artifacts.require("MemoryTest");
 
 module.exports = function(deployer) {
   deployer.deploy(InvalidTest);
@@ -10,5 +9,4 @@ module.exports = function(deployer) {
   deployer.link(InvalidLib, InvalidLibTest);
   deployer.deploy(InvalidLibTest);
   deployer.deploy(ConstructorTest);
-  deployer.deploy(MemoryTest);
 };
