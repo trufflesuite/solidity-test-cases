1377a23bc592fa0c104673f2731e327fc307d8da
Harry Altman <harry.j.altman@gmail.com>
2019-06-05 00:13:47 -0400

    Ooops, missed some stuff

--- events-test/migrations/2_deploy_contracts.js
+++ events-test/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var EventsTest = artifacts.require("EventsTest");
+var EventOptionsTest = artifacts.require("EventOptionsTest");
 
 module.exports = function(deployer) {
   deployer.deploy(EventsTest);
+  deployer.deploy(EventOptionsTest);
 };
