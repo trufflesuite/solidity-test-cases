4eece4ba813fb96e97a282d3991dde140728adf7
Harry Altman <harry.j.altman@gmail.com>
2019-06-05 00:53:21 -0400

    Add deploy for AnonTest

--- events-test/migrations/2_deploy_contracts.js
+++ events-test/migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 var EventsTest = artifacts.require("EventsTest");
 var EventOptionsTest = artifacts.require("EventOptionsTest");
+var AnonTest = artifacts.require("AnonTest");
 
 module.exports = function(deployer) {
   deployer.deploy(EventsTest);
   deployer.deploy(EventOptionsTest);
+  deployer.deploy(AnonTest, 3, 35);
 };
