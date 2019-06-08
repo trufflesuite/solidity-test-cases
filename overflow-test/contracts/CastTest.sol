8a108b462a42d708e20c32ab88fa3814c1f4b4c0
Harry Altman <harry.j.altman@gmail.com>
2019-06-08 02:09:04 -0400

    Add test of overflows from casts

---
+++ overflow-test/contracts/CastTest.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.5.9;
+
+contract CastTest {
+
+  enum Ternary {
+    Yes, No, MaybeSo
+  }
+
+  event enumEvent(Ternary indexed);
+  event addressEvent(address indexed);
+  event contractEvent(CastTest indexed);
+
+  function enumTest() public {
+    uint8 byte1 = 0xff;
+    uint8 byte2 = 1;
+    uint8 sum = byte1 + byte2;
+    Ternary cast = Ternary(sum);
+    emit enumEvent(cast);
+  }
+
+  function addressTest() public {
+    uint160 integer1 = 0x00ffffffffffffffffffffffffffffffffffffffff;
+    uint160 integer2 = 1;
+    uint160 sum = integer1 + integer2;
+    address cast = address(sum);
+    emit addressEvent(cast);
+  }
+
+  function contractTest() public {
+    uint160 integer1 = 0x00ffffffffffffffffffffffffffffffffffffffff;
+    uint160 integer2 = 1;
+    uint160 sum = integer1 + integer2;
+    CastTest cast = CastTest(sum);
+    emit contractEvent(cast);
+  }
+}
--- overflow-test/migrations/2_deploy_contracts.js
+++ overflow-test/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var OverflowTest = artifacts.require("OverflowTest");
+var CastTest = artifacts.require("CastTest");
 
 module.exports = function(deployer) {
   deployer.deploy(OverflowTest);
+  deployer.deploy(CastTest);
 };
