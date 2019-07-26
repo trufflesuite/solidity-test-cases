d24e8c2880e46e6abd8d287817537eee93301ff2
Harry Altman <harry.j.altman@gmail.com>
2019-07-26 14:36:41 -0400

    Add test of super-skipping

--- super-call-test/contracts/MultiSuper.sol
+++ super-call-test/contracts/MultiSuper.sol
@@ -5,6 +5,14 @@ contract O {
     emit Name("O");
   }
 
+  function wayUpThere() public {
+    emit Name("O");
+  }
+
+  function skips() public {
+    emit Name("O");
+  }
+
   event Name(string);
 }
 
@@ -13,12 +21,22 @@ contract C is O {
     emit Name("C");
     super.run();
   }
+
+  function skips() public {
+    emit Name("C");
+    super.skips();
+  }
 }
 
 contract A is O {
   function run() public {
     emit Name("A");
     super.run();
+  }
+
+  function skips() public {
+    emit Name("A");
+    super.skips();
   }
 }
 
@@ -27,12 +45,21 @@ contract B is O {
     emit Name("B");
     super.run();
   }
+
+  function onlySome() public {
+    emit Name("B");
+  }
 }
 
 contract D is O {
   function run() public {
     emit Name("D");
     super.run();
+  }
+
+  function skips() public {
+    emit Name("D");
+    super.skips();
   }
 }
 
@@ -41,12 +68,28 @@ contract E is O {
     emit Name("E");
     super.run();
   }
+
+  function skips() public {
+    emit Name("E");
+    super.skips();
+  }
 }
 
 contract K1 is C, B, A {
   function run() public {
     emit Name("K1");
-    super.run();
+    function() internal toRun = super.run;
+    toRun();
+  }
+
+  function onlySome() public {
+    emit Name("K1");
+    super.onlySome();
+  }
+
+  function skips() public {
+    emit Name("K1");
+    super.skips();
   }
 }
 
@@ -55,6 +98,16 @@ contract K2 is E, B, D {
     emit Name("K2");
     super.run();
   }
+
+  function onlySome() public {
+    emit Name("K2");
+    super.onlySome();
+  }
+
+  function skips() public {
+    emit Name("K2");
+    super.skips();
+  }
 }
 
 contract K3 is A, D {
@@ -62,11 +115,38 @@ contract K3 is A, D {
     emit Name("K3");
     super.run();
   }
+
+  /* not allowed
+  function onlySome() public {
+    emit Name("K3");
+    super.onlySome();
+  }
+  */
+
+  function skips() public {
+    emit Name("K3");
+    super.skips();
+  }
 }
 
 contract Z is K3, K2, K1 {
   function run() public {
     emit Name("Z");
     super.run();
+  }
+
+  function notRun() public {
+    emit Name("Z");
+    K1.run();
+  }
+
+  function wayUpThere() public {
+    emit Name("Z");
+    super.wayUpThere();
+  }
+
+  function skips() public {
+    emit Name("Z");
+    super.skips();
   }
 }
--- super-call-test/migrations/2_deploy_contracts.js
+++ super-call-test/migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
 var Derived = artifacts.require("Derived");
+var Z = artifacts.require("Z");
+var K1 = artifacts.require("K1");
+var K2 = artifacts.require("K2");
+var K3 = artifacts.require("K3");
 
 module.exports = function(deployer) {
   deployer.deploy(Derived);
+  deployer.deploy(Z);
+  deployer.deploy(K1);
+  deployer.deploy(K2);
+  deployer.deploy(K3);
 };
