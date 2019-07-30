793ee9dffde694dddd453cd12c2e7ebd0a943470
Harry Altman <harry.j.altman@gmail.com>
2019-07-30 19:45:05 -0400

    Add another compiler crash

---
+++ compile-crash/contracts/TransferTest.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+contract TransferTest {
+  function() external payable {
+    //address(this).transfer;
+    //address(this).send;
+  }
+}
--- compile-crash/truffle.js
+++ compile-crash/truffle.js
@@ -12,7 +12,7 @@ module.exports = {
   {
     solc:
     {
-      version: "0.5.3",
+      version: "^0.5.3",
       memoize: false
     }
   }
