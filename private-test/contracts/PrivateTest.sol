7aa7d73ced7fac34a6c0c03f81d0e3699495ca62
Harry Altman <harry.j.altman@gmail.com>
2019-09-27 16:38:50 -0400

    Add test of hiding private variables

---
+++ private-test/contracts/PrivateTest.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.5.11;
+
+contract Base {
+  uint internal visible;
+  uint private hidden;
+  uint public impossibleToMiss;
+
+  constructor() public {
+    visible = 99;
+    hidden = 1;
+    impossibleToMiss = 99999;
+  }
+}
+
+contract Derived is Base {
+  uint internal myVisible;
+  uint private myHidden;
+  uint public myImpossibleToMiss;
+
+  function run() public {
+    myVisible = 102;
+    myHidden = 27;
+    myImpossibleToMiss = 88888;
+    visible = 47;
+    impossibleToMiss = 9999999999;
+  }
+}
