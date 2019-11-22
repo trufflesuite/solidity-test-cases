04579ef8b3494930ee2b0cc113dfa77691b45f35
Harry Altman <harry.j.altman@gmail.com>
2019-11-22 02:50:12 -0500

    Remove test contract from migrations

--- nimprod/migrations/2_deploy_contracts.js
+++ nimprod/migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 var NimProd = artifacts.require("NimProd");
-var NimProdTester = artifacts.require("NimProdTester");
 
 module.exports = function(deployer) {
   deployer.deploy(NimProd);
-  deployer.deploy(NimProdTester);
 };
