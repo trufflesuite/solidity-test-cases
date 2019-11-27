c92c5efa16d1a0dd7e87cc1d06e4db336a7ebc48
Harry Altman <harry.j.altman@gmail.com>
2019-11-27 16:08:54 -0500

    Add more with heritable events

--- events-test/contracts/CrossEventTest.sol
+++ events-test/contracts/CrossEventTest.sol
@@ -2,11 +2,27 @@ pragma solidity ^0.5.13;
 
 contract Base {
   event baseEvent(uint);
+  event bothEvent(uint);
 }
 
 contract Derived is Base {
-  event baseEvent(uint indexed);
+  event bothEvent(uint indexed);
+  event derivedEvent(uint indexed);
   function run() public {
     emit baseEvent(1);
+    emit Base.baseEvent(2);
+    emit bothEvent(3);
+    emit Base.bothEvent(4);
+    emit Derived.bothEvent(5);
+    emit derivedEvent(6);
+    emit Derived.derivedEvent(7);
   }
+}
+
+contract Other {
+  /*
+  function run() public {
+    emit Base.baseEvent(1); //doesn't work!
+  }
+  */
 }
