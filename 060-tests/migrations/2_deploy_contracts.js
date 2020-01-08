f2faf4a44c9703b347e23d8203b0fc545808846e
Harry Altman <harry.j.altman@gmail.com>
2020-01-07 20:30:22 -0500

    Remove deployment of nonexistent contract

--- 060-tests/migrations/2_deploy_contracts.js
+++ 060-tests/migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-var Test = artifacts.require("Test");
 var ModifierTest = artifacts.require("ModifierTest");
 
 module.exports = function(deployer) {
-  deployer.deploy(Test);
   deployer.deploy(ModifierTest);
 };
