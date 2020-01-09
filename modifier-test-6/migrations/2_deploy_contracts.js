a5fc83d79bdc0f924a289b75d9b631d4b9258118
Harry Altman <harry.j.altman@gmail.com>
2020-01-09 01:23:02 -0500

    Deploy DepthTest

--- modifier-test-6/migrations/2_deploy_contracts.js
+++ modifier-test-6/migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 var ModifierTest = artifacts.require("ModifierTest");
 var ConstructorJunkTest = artifacts.require("ConstructorJunkTest");
+var DepthTest = artifacts.require("DepthTest");
 
 module.exports = function(deployer) {
   deployer.deploy(ModifierTest);
   deployer.deploy(ConstructorJunkTest);
+  deployer.deploy(DepthTest);
 };
