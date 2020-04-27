a75135cea337c67d11ab55d484ec0e7abdc32908
Harry Altman <harry.j.altman@gmail.com>
2020-04-27 17:37:23 -0400

    Add test of .on

--- stacktrace-test/test/test.js
+++ stacktrace-test/test/test.js
@@ -2,6 +2,12 @@ const util = require("util");
 const StacktraceTest = artifacts.require("StacktraceTest");
 
 contract("StacktraceTest", function(accounts) {
+  /*
+  it("succeeds & prints txHash", async function() {
+    let instance = await StacktraceTest.deployed();
+    instance.run(12).on("transactionHash", console.log);
+  });
+  */
   it("fails! hahahaha", async function() {
     let instance = await StacktraceTest.deployed();
     await instance.run(2); //leaves a message
