5f0d9a464cf9d95974e29e702883205ea2e29570
Harry Altman <harry.j.altman@gmail.com>
2020-04-30 00:51:49 -0400

    Add v5 assembly test

---
+++ old-assembly-test/contracts/ScopesTest.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.0;
+
+contract ScopesTest {
+  function doNothing() public {
+    uint woozle;
+    assembly {
+      function noop() {
+      }
+      noop()
+      let x := 1
+      { 
+        let y := 2
+	y := add(y,1)
+      }
+      for { let i := 3 x := 40 } lt(i, 5) { i := add(i, 1) } {
+        log1(0, 0, i)
+      }
+      function complex(a, b) -> c, d {
+        c := add(a,b)
+        d := sub(a,b)
+      }
+      let z, w := complex(7, 8)
+      woozle := address()
+      stop()
+      selfdestruct(woozle) //don't actually selfdestruct
+    }
+  }
+}
