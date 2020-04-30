97df2b28c8f0d9733d0d81b14ae949e30a99ddc1
Harry Altman <harry.j.altman@gmail.com>
2020-04-29 20:09:33 -0400

    Add test of Yul scopes

---
+++ assembly-test/ScopesTest.sol
@@ -0,0 +1,20 @@
+contract ScopesTest {
+  function doNothing() public {
+    assembly {
+      function noop() {
+      }
+      noop()
+      let x := 1
+      { 
+        let y := 2
+      }
+      for { let i := 3 } lt(i, 5) { i := add(i, 1) } {
+        log1(0, 0, i)
+      }
+      function complex(a, b) -> c, d {
+        c := add(a,b)
+        d := sub(a,b)
+      }
+    }
+  }
+}
