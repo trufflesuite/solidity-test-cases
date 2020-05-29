1f68a6b7f945b13ab181c441d10034617833c00c
Harry Altman <harry.j.altman@gmail.com>
2020-05-29 18:45:00 -0400

    Add test of Yul objects in Sol (it's not allowed)

---
+++ assembly-test/contracts/ObjectTest.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.6.0;
+
+contract ObjectTest {
+
+  function run() public {
+    assembly {
+      //objects not allowed in this context, seems!
+      /*
+      object "thingy" {
+        code {
+          stop()
+        }
+        data hex"00"
+      }
+      */
+    }
+  }
+}
