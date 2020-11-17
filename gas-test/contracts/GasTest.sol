d79efac218a65c118e872c199455d63b0d1ea03b
Harry Altman <harry.j.altman@gmail.com>
2020-11-17 02:13:21 -0500

    Add more tests of how gas works

--- gas-test/contracts/GasTest.sol
+++ gas-test/contracts/GasTest.sol
@@ -5,19 +5,29 @@ contract GasTest {
 
   event Dummy();
 
+  uint256[3] words;
+  Temporary temp;
+
+  constructor() payable {
+  }
+
   function run() public {
     emit Dummy();
+    this.succeed{gas: gasleft() - 900}();
+    emit Dummy();
+    try this.fail{gas: gasleft() - 900}() {
+    emit Dummy();
+    } catch (bytes memory) {
+    }
+    emit Dummy();
+    try this.failbad{gas: gasleft() - 900}() {
+    } catch (bytes memory) {
+    }
+    emit Dummy();
+  }
+
+  function simple() public {
     this.succeed();
-    emit Dummy();
-    try this.fail() {
-    emit Dummy();
-    } catch(bytes memory) {
-    }
-    emit Dummy();
-    try this.failbad() {
-    } catch(bytes memory) {
-    }
-    emit Dummy();
   }
 
   function succeed() public {
@@ -32,5 +42,86 @@ contract GasTest {
   function failbad() public {
     emit Dummy();
     assert(false);
+  }
+
+  function prepare() public {
+    bool unready;
+    assembly {
+      unready := iszero(extcodesize(sload(temp.slot)))
+    }
+    if (unready) {
+      temp = new Temporary{salt: hex""}();
+    }
+    if (words[0] == 0) {
+      words[0] = 1;
+      words[1] = 1;
+      words[2] = 1;
+    }
+  }
+
+  function doesthiswork() public {
+    try this.justatest{value: 1}() {
+    } catch (bytes memory) {
+    }
+  }
+
+  function justatest() public payable {
+  }
+
+  function sdtest() public {
+    emit Dummy();
+    temp.boom();
+    emit Dummy();
+  }
+
+  function deletetest() public {
+    emit Dummy();
+    delete words;
+    emit Dummy();
+  }
+
+  function rundeletetest() public {
+    emit Dummy();
+    this.deletetest();
+    emit Dummy();
+  }
+
+  function payme() public payable {
+    emit Dummy();
+  }
+
+  function stiptest() public {
+    emit Dummy();
+    this.payme{value: 1}();
+    emit Dummy();
+    new Temporary{value: 1}();
+    emit Dummy();
+  }
+
+  function makeSink() public {
+    new Sink();
+  }
+}
+
+contract Temporary {
+
+  event Dummy();
+
+  function confirm() public {
+  }
+
+  constructor() payable {
+  }
+
+  function boom() public {
+    emit Dummy();
+    selfdestruct(tx.origin);
+  }
+}
+
+contract Sink {
+  constructor() payable {
+  }
+  fallback() external payable {
   }
 }
--- gas-test/migrations/2_deploy_contracts.js
+++ gas-test/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var GasTest = artifacts.require("GasTest");
 
 module.exports = function(deployer) {
-  deployer.deploy(GasTest);
+  deployer.deploy(GasTest, {value: 100});
 };
