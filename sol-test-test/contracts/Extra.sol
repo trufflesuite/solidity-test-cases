d9869f1481c5634e925058ce3efe9e376a689521
Harry Altman <harry.j.altman@gmail.com>
2020-12-22 19:12:29 -0500

    Test Truffle imports

--- sol-test-test/contracts/Extra.sol
+++ sol-test-test/contracts/Extra.sol
@@ -1,2 +1,3 @@
+pragma solidity ^0.5.10;
 contract Extra {
 }
--- sol-test-test/test/SolTestTester.sol
+++ sol-test-test/test/SolTestTester.sol
@@ -1,11 +1,13 @@
 pragma solidity ^0.5.10;
 
 import "../contracts/SolTestTest.sol";
+import "truffle/Assert.sol";
 
 contract SolTestTester is SolTestTest {
 
   function test1() public {
     this.run(true);
+    Assert.equal(uint(1), 1, "");
   }
 
   function test2() public {
