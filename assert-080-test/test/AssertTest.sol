59d92091fdec0bec1798d7140a98f2d0b36ab11a
Harry Altman <harry.j.altman@gmail.com>
2021-01-26 16:45:45 -0500

    Add tests of importing other Truffle sources

--- assert-080-test/test/AssertTest.sol
+++ assert-080-test/test/AssertTest.sol
@@ -1,7 +1,9 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "truffle/AssertInt.sol";
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "truffle/NewSafeSend.sol";
 
 contract AssertTest {
   function test() public {
