b6ac11dfb29bb77df8d6b79ae628ec14d7f72155
Harry Altman <harry.j.altman@gmail.com>
2021-03-25 22:26:29 -0400

    Add Vyper test for decimals

---
+++ encoder-tests/contracts/DecimalTest.vy
@@ -0,0 +1,3 @@
+@external
+def takesDecimal(a: decimal):
+    pass
--- encoder-tests/migrations/2_deploy_contracts.js
+++ encoder-tests/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var EncoderTest = artifacts.require("EncoderTest");
+var DecimalTest = artifacts.require("DecimalTest");
 
 module.exports = function(deployer) {
   deployer.deploy(EncoderTest, 2);
+  deployer.deploy(DecimalTest);
 };
