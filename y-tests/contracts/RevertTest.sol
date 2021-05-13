b508f9885731a20ee5e7a65322819c758a0e7efa
Harry Altman <harry.j.altman@gmail.com>
2021-05-12 22:53:18 -0400

    Add test of y/Y commands

---
+++ y-tests/contracts/RevertTest.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract RevertTest {
+
+  event Here();
+
+  function run() public {
+    emit Here();
+    try this.run2() {
+    } catch {
+    }
+    revert();
+  }
+
+  function run2() public {
+    emit Here();
+    this.run3();
+  }
+
+  function run3() public {
+    emit Here();
+    revert();
+  }
+
+  function ok() public {
+    emit Here();
+  }
+}
---
+++ y-tests/migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var RevertTest = artifacts.require("RevertTest");
+
+module.exports = function(deployer) {
+  deployer.deploy(RevertTest);
+};
