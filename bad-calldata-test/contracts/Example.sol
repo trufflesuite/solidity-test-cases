29e8760d971669dd36f88030e7ac17d9f6e27f26
Harry Altman <harry.j.altman@gmail.com>
2021-05-21 02:08:59 -0400

    Add more tests of pop problems

--- bad-calldata-test/contracts/Example.sol
+++ bad-calldata-test/contracts/Example.sol
@@ -26,4 +26,100 @@ contract Example {
         }
         return retval;
     }
+
+    function ifTest(uint[] calldata data) external returns (bool) {
+        bool retval;
+        if (!retval) {
+            uint something = 3;
+            retval = true;
+        }
+        return retval;
+    }
+
+    function whileTest(uint[] calldata data) external returns (bool) {
+        bool retval;
+        while (!retval) {
+            uint something = 3;
+            retval = true;
+        }
+        return retval;
+    }
+
+    function doTest(uint[] calldata data) external returns (bool) {
+        bool retval;
+        do {
+            uint something = 3;
+            retval = true;
+        } while (!retval);
+        return retval;
+    }
+
+    function nestTest(uint[] calldata data) external returns (bool) {
+        bool retval;
+        if (!retval) {
+            {
+                uint something = 3;
+                retval = true;
+            }
+        }
+        return retval;
+    }
+
+    function bareNestTest(uint[] calldata data) external returns (bool) {
+        bool retval;
+        {
+            {
+                uint something = 3;
+                retval = true;
+            }
+        }
+        return retval;
+    }
+
+    function assemblyTest(uint[] calldata data) external returns (bool) {
+        bool retval;
+        assembly {
+            let something := 3
+            retval := true
+        }
+        return retval;
+    }
+
+    function assemblyBlockTest(uint[] calldata data) external returns (bool) {
+        bool retval;
+        assembly {
+            {
+                let something := 3
+                retval := true
+            }
+        }
+        return retval;
+    }
+
+    function assemblyIfTest(uint[] calldata data) external returns (bool) {
+        bool retval;
+        assembly {
+            if iszero(retval) {
+                let something := 3
+                retval := true
+            }
+        }
+        return retval;
+    }
+
+    function assemblyFnTest(uint[] calldata data) external returns (bool) {
+        assembly {
+            function assemblyFn(offset, length) -> retval {
+                {
+                    let something := 3
+                    retval := true
+                }
+                retval := true
+            }
+            let returned := assemblyFn(data.offset, data.length)
+            let freelocation := mload(0x40)
+            mstore(freelocation, returned)
+            return(freelocation, 0x20)
+        }
+    }
 }
