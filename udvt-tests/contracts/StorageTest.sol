3c2194b0991b3580521ca160640ae60321832a16
Harry Altman <harry.j.altman@gmail.com>
2021-12-02 20:16:58 -0500

    Add more tests of UDVTs

--- udvt-tests/contracts/StorageTest.sol
+++ udvt-tests/contracts/StorageTest.sol
@@ -6,12 +6,18 @@ contract StorageTest {
   MyInt first;
   MyInt second;
   MyInt immutable third = MyInt.wrap(-3);
+  MyInt[] arr;
+  MyInt[2] staticArr;
 
   event Done(int8);
 
   function run() public {
     first = MyInt.wrap(-1);
     second = MyInt.wrap(-2);
+    arr.push(MyInt.wrap(-4));
+    arr.push(MyInt.wrap(-5));
+    arr.push(MyInt.wrap(-6));
+    staticArr = [MyInt.wrap(-7), MyInt.wrap(-8)];
     emit Done(MyInt.unwrap(third));
   }
 
