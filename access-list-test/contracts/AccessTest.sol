bd955d40d18d577f392d5e4f42ed59f088a236b1
Harry Altman <harry.j.altman@gmail.com>
2021-12-06 16:47:59 -0500

    Add second storage slot for testing

--- access-list-test/contracts/AccessTest.sol
+++ access-list-test/contracts/AccessTest.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.0;
 contract AccessTest {
   Auxiliary immutable public aux;
   uint public it = 1;
+  uint public also = 1;
   constructor(Auxiliary _aux) {
     aux = _aux;
   }
 
   function run() public {
     it++;
+    also++;
     aux.run();
   }
 }
