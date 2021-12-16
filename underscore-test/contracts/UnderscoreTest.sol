6790d70d402fabb09b9f7c7e6bcc61d1cc062b42
Harry Altman <harry.j.altman@gmail.com>
2021-12-15 20:34:27 -0500

    Further test underscores

--- underscore-test/contracts/UnderscoreTest.sol
+++ underscore-test/contracts/UnderscoreTest.sol
@@ -4,10 +4,15 @@ pragma solidity ^0.8.0;
 contract UnderscoreTest {
 
   bytes2 pair;
+  uint256 num;
 
   function run() public {
     pair = 0xfff_f;
     //pair = hex"fff_f";
     pair = hex"ff_ff";
+    num = 1_1;
+    num = 1_1e1_0;
+    //num = 1_e10;
+    //num = 1e_10;
   }
 }
