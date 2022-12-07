d3826271c1df26d8b13dd67029b9b27f75f6d1e2
Harry Altman <harry.j.altman@gmail.com>
2022-12-07 03:36:41 -0500

    Get reverse record to work (...assuming some changes to Truffle)

--- ens-test/migrations/2_deploy_contracts.js
+++ ens-test/migrations/2_deploy_contracts.js
@@ -3,7 +3,14 @@ const EnsTest = artifacts.require("EnsTest");
 module.exports = async function(deployer, _, accounts) {
   const name = "works.eth";
   await deployer.ens.setAddress(name, accounts[0], { from: accounts[0] });
-  await deployer.ens.ensjs.setReverseRecord(name); //hopefully this sets it for accounts[0]??
+  //confirm
+  //console.log("addr for 1st: %s", await deployer.ens.ensjs.name(name).getAddress());
+  //console.log("owner for sym: %s", await deployer.ens.ensjs.name("addr.reverse").getOwner());
   const registryAddress = deployer.ens.devRegistry.address;
+  //console.log("reg addr: %s", registryAddress);
+  const rcpt = await deployer.ens.ensjs.setReverseRecord(name); //hopefully this sets it for accounts[0]??
+  //confirm
+  //console.log("rcpt: %O", rcpt);
+  //console.log("name for 1st: %o", await deployer.ens.ensjs.getName(accounts[0]));
   await deployer.deploy(EnsTest, accounts[0], accounts[1], registryAddress);
 };
