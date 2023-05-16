ee6839a7f431fac725e08d0a22f750ea2180c6c4
Harry Altman <harry.j.altman@gmail.com>
2023-05-15 22:41:13 -0400

    Deploy OverloadTest

--- call-test/migrations/2_deploy_contracts.js
+++ call-test/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var CallTest = artifacts.require("CallTest");
+var OverloadTest = artifacts.require("OverloadTest");
 
 module.exports = function(deployer) {
   deployer.deploy(CallTest);
+  deployer.deploy(OverloadTest);
 };
